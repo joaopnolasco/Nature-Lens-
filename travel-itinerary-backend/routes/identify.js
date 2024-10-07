a1ef9179adebabe9f75656beb23842585bc64956
Ayrton <afg@cin.ufpe.br>
2024-10-07 13:04:52 -0300

    backend

--- travel-itinerary-backend/routes/identify.js
+++ travel-itinerary-backend/routes/identify.js
@@ -20,6 +20,7 @@ const generativeVisionModel = vertexAI.getGenerativeModel({ model: modelId });
 
 // Função para buscar dados da espécie no GBIF
 async function getSpeciesData(speciesName) {
+  console.log('Buscando dados da espécie:', speciesName);
   const response = await fetch(`https://api.gbif.org/v1/species?name=${encodeURIComponent(speciesName)}`);
   const data = await response.json();
   return data.results[0]; // Retorna o primeiro resultado encontrado
@@ -38,7 +39,18 @@ router.post('/', upload.single('image'), async (req, res) => {
     };
 
     const textPart = {
-      text: 'Caso a imagem esteja relacionada com animais ou plantas, identifique a espécie nesta imagem e forneça detalhes sobre essa espécie, caso não esteja relacionado com animais ou plantas retorne "Envie a imagem de uma espécie de planta ou anaimal".',
+      text: `
+  Ao receber uma imagem, siga este padrão de resposta, mas sem numerar ou listar os pontos explicitamente. A resposta deve ser fluida e natural:
+
+  1. Inicie identificando se a imagem é de uma planta ou animal, e descreva brevemente o organismo, mencionando o nome comum.
+  2. Informe o nome científico em itálico como parte da explicação.
+  3. Explique o habitat natural da espécie, mencionando onde ela pode ser encontrada no mundo.
+  4. Descreva as características distintivas, como tamanho, cor, formato e outras particularidades importantes, de forma fluida e sem listar os itens.
+  5. Adicione curiosidades ou fatos interessantes sobre a espécie, integrando essas informações ao longo da explicação.
+  6. A linguagem deve ser clara, acessível e informativa, como se estivesse explicando de maneira casual para um amigo.
+
+  Se a imagem não estiver relacionada a plantas ou animais, informe ao usuário educadamente e sugira que ele selecione uma nova imagem.
+  `,
     };
 
     const request = {
@@ -59,8 +71,9 @@ router.post('/', upload.single('image'), async (req, res) => {
 
     // Limpar o arquivo de imagem temporário
     await fs.unlink(imagePath);
+    console.log('Arquivo de speciesData', speciesData);
 
-    res.json({ description, speciesData });
+    res.json({  description, speciesData });
   } catch (error) {
     console.error('Erro ao identificar a espécie:', error);
     res.status(500).json({
